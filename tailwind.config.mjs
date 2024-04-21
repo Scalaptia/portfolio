af15d3431662faacef2af5534128a38f62a01fb3
Scalaptia <fernando@haroware.com>
2024-04-20 20:45:25 -0700

    Update homepage

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#FD8D75",
+                secondary: "#ADC7C6",
+                text: "#412C47",
+                background: "#FDF7E7",
+            },
+            fontFamily: {
+                "ubuntu-mono": ["Ubuntu Mono", "monospace"],
+                "black-han-sans": ["Black Han Sans", "sans-serif"],
+                "open-sans": ["Open Sans", "sans-serif"],
+            },
+        },
+        variants: {},
+        plugins: [],
+    },
+};
