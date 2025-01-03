818414ee555576b67957764858b79b48230c4cb1
Fernando Haro <fernando@haroware.com>
2025-01-02 19:31:41 -0800

    Language picker mobile

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,10 +7,16 @@ export const defaultLang = 'en';
 
 export const ui = {
     en: {
+        'home': 'home',
+        'blog': 'blog',
+        'error': 'error',
         'nav.home': 'home',
         'nav.blog': 'blog',
     },
     es: {
+        'home': 'inicio',
+        'blog': 'blog',
+        'error': 'error',
         'nav.home': 'inicio',
         'nav.blog': 'blog',
     },
