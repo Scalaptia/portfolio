a032e0af1178211dd0f8f58a5fb9df17592c40c0
Fernando Haro <fernando@haroware.com>
2025-01-02 21:20:15 -0800

    Translate to spanish

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 import { Copy } from "lucide-react";
 
-export default function CopyToClipboard({ text }: { text: string }) {
+interface CopyToClipboardProps {
+    text: string;
+    copiedText: string;
+}
+
+export default function CopyToClipboard({ text, copiedText }: CopyToClipboardProps) {
     const [showPopup, setShowPopup] = useState(false);
 
     const handleClick = async () => {
@@ -18,15 +23,9 @@ export default function CopyToClipboard({ text }: { text: string }) {
             >
                 <Copy className="w-5 h-5" />
             </button>
-            {showPopup ? (
-                <div className="pointer-events-none absolute font-open-sans text-center -top-10 -left-32 md:left-0 bg-white text-text border-2 border-text p-2 z-10 w-[19ch] transition duration-100 ease-in-out opacity-100">
-                    Copied to clipboard!
-                </div>
-            ) : (
-                <div className="pointer-events-none absolute font-open-sans text-center -top-10 -left-32 md:left-0 bg-white text-text border-2 border-text p-2 z-10 w-[19ch] transition duration-100 ease-in-out opacity-0">
-                    Copied to clipboard!
-                </div>
-            )}
+            <div className={`pointer-events-none absolute font-open-sans text-center -top-10 -left-32 md:left-0 bg-white text-text border-2 border-text p-2 z-10 inline-block w-auto transition duration-100 ease-in-out whitespace-nowrap overflow-hidden text-overflow-ellipsis ${showPopup ? 'opacity-100' : 'opacity-0'}`}>
+                {copiedText}
+            </div>
         </div>
     );
 }
